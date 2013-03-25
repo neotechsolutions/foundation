8a167d9a1854a4278a21198151f4925466c5025f
Matthieu Klotz <matthieu.klotz@mattk.info>
2013-03-25 16:06:48 +0100

    Add base solution

---
+++ Sources/NeotechSolutions.Foundation/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Neotech Solutions">
+// Copyright (c) 2013, Neotech Solutions
+// All rights reserved.
+// See License.txt in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NeotechSolutions.Foundation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Neotech Solutions")]
+[assembly: AssemblyProduct("NeotechSolutions.Foundation")]
+[assembly: AssemblyCopyright("Copyright © 2013 Neotech Solutions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguageAttribute("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("801ccced-e9b2-41c1-a9c2-dbddec4f832b")]
